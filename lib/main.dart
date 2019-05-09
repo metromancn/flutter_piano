aed8ef1b0adbf55b88bdc12ff9ebc46274abe3ad
qiuping_wu <qiuping_wu@ot.olympus.co.jp>
2019-05-09 11:14:56 +0900

    draw octave

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class _MyAppState extends State<MyApp> {
   double _widthRatio = 0.0;
   double get keyWidth => 80 + (80 * _widthRatio);
 
+
   @override
   void initState() {
     FlutterMidi.unmute();
@@ -36,10 +37,62 @@ class _MyAppState extends State<MyApp> {
             scrollDirection: Axis.horizontal,
             itemBuilder: (BuildContext context, int index) {
               final int i = index * 12;
-              return Container();
+              return SafeArea(
+                child: Stack(children: <Widget>[
+                  Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                    _buildKey(24 + i, false),
+                    _buildKey(26 + i, false),
+                    _buildKey(28 + i, false),
+                    _buildKey(29 + i, false),
+                    _buildKey(31 + i, false),
+                    _buildKey(33 + i, false),
+                    _buildKey(35 + i, false),
+                  ]),
+                  Positioned(
+                      left: 0.0,
+                      right: 0.0,
+                      bottom: 100,
+                      top: 0.0,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(width: keyWidth * .5),
+                            _buildKey(25 + i, true),
+                            _buildKey(27 + i, true),
+                            Container(width: keyWidth),
+                            _buildKey(30 + i, true),
+                            _buildKey(32 + i, true),
+                            _buildKey(34 + i, true),
+                            Container(width: keyWidth * .5),
+                          ])),
+                ]),
+              );
             },
         ),
       ),
     );
   }
+
+  Widget _buildKey(int midi, bool accidental) {
+    if (accidental) {
+      return Container(
+          width: keyWidth,
+          color: Colors.black,
+          margin: EdgeInsets.symmetric(horizontal: 2.0),
+          padding: EdgeInsets.symmetric(horizontal: keyWidth * .1),
+          child: Material(
+            elevation: 6.0,
+            borderRadius: borderRadius,
+            shadowColor: Color(0x802196F3),
+          ));
+    }
+    return Container(
+        width: keyWidth,
+        color: Colors.white,
+        margin: EdgeInsets.symmetric(horizontal: 2.0));
+  }
 }
+
+const BorderRadiusGeometry borderRadius = BorderRadius.only(
+    bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0));
